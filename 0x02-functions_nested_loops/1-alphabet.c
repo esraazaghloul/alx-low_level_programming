feae1d51976c99441bab5a05b73f2b01d0f22f65
esraazaghloul <esraazaghloul@live.com>
2023-10-10 16:47:39 -0700

    34

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
