877bd8d73ebcf34d96f6d3131fe5b71f6f20a970
esraazaghloul <esraazaghloul@live.com>
2023-10-10 15:18:02 -0700

    19

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include main.h
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	Return 0;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main.h
+#define main.h
+
+#include <stdio.h>
+
+#endif
