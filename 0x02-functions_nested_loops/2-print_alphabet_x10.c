1e54159142dbdf422030022f00d0314b98ade0a1
esraazaghloul <esraazaghloul@live.com>
2023-10-10 17:09:45 -0700

    36

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+	int i = 1;
+
+	for (i <= 10 ; i++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	i = i + 1;
+	}
+	return (0);
+}
