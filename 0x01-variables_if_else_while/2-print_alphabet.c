75a365ea8bbe09904a13ccd5b3479027a6e74912
esraazaghloul <esraazaghloul@live.com>
2023-10-11 20:06:41 -0700

    39

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pint
+ *
+ * Description: A C program
+ *
+ * Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c, '\n');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
