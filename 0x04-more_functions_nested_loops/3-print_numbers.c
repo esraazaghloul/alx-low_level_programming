7a6297a7036a9632c08fff457835b801b954787b
esraazaghloul <esraazaghloul@live.com>
2023-10-12 16:24:33 -0700

    4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,16 @@
  *
  * Return: void
  */
+
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
-	_putchar('0' + i);
+	do
+	{
+	_putchar(i + 48);
+	i++;
+	}
+	while (i < 10);
 	_putchar('\n');
 }
