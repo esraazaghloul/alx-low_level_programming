24fd051464a8464eb91510ac12b29b6326f15a73
esraazaghloul <esraazaghloul@live.com>
2023-10-12 17:14:49 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,11 +12,9 @@ void print_numbers(void)
 {
 	int num = 0;
 
-	do
-	{
-		_putchar(num + 48);
-		num++;
-	}
-	while (num >= 0 && num <= 9);
+	do {
+	_putchar(num + 48);
+	num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
