4f0c953623ad7c790ab9957df483ed9e739a7495
esraazaghloul <esraazaghloul@live.com>
2023-10-12 23:20:58 -0700

    5

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ *
+ * @n: The number of underscores to be printed.
+ *
+ * Return: void
+ *
+ * Description: a C program
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
