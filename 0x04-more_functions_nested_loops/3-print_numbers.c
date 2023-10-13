b1067304b23f36dee80790ffea6d1f5c876b8a3b
esraazaghloul <esraazaghloul@live.com>
2023-10-12 17:09:50 -0700

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - prints numbers from 0 to 9
@@ -15,6 +15,6 @@ void print_numbers(void)
 	do {
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9)
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
