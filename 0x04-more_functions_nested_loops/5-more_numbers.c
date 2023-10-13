fdd6f7b9d9980a213472c38c27224b6076b125c6
esraazaghloul <esraazaghloul@live.com>
2023-10-12 18:16:05 -0700

    4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,16 @@ void more_numbers(void)
 
 	for (r = 1; r <= 10; r++)
 	{
-		n = c;
-		if (c > 9)
+		for (c = 0; c <= 14; c++)
 		{
-			_putchar(1 + 48);
-			n = count % 10;
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+				}
+			_putchar(n + 48);
 		}
-		_putchar(n + 48);
-	}
 	_putchar('\n');
+	}
 }
