5ed4a8a16c1e42f8b5de1deff7e1c8d52c5e41d0
esraazaghloul <esraazaghloul@live.com>
2023-10-16 14:23:34 -0700

    3

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_rev - prints a string, in reverse, followed by a new line
  *
@@ -13,7 +15,7 @@ void print_rev(char *s)
 
 	while (s[length] != '\0')
 		length++;
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
