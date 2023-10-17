505d95cf7cf09d3d5e0db34ca3896620d6db0f86
esraazaghloul <esraazaghloul@live.com>
2023-10-16 17:19:43 -0700

    3

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random passwords for 101-crackme
+ *
+ * Return: 0 if successful, 1 otherwise
+*/
+
+int main(void)
+{
+	int i;
+	char password[9]
+
+	srand(time(NULL));
+
+	for (i = 0; i < 8; i++)
+		password[i] = 'a' + rand() % 26;
+	password[8] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
+
