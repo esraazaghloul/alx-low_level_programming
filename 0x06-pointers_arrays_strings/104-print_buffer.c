e13eea4bbba7543c3792afc0e320fba85c96643c
esraazaghloul <esraazaghloul@live.com>
2023-10-19 16:02:44 -0700

    3

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ *
+ * @b: pointer to the buffer
+ *
+ * @size: size of the buffer
+ *
+ * Return: Nothing.
+*/
+
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", b[i + j]);
+			else
+				printf("  ");
+			if (j % 2)
+				_putchar(' ');
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				{
+					if (b[i + j] < 32 || b[i + j] > 126)
+						_putchar('.');
+					else
+						_putchar(b[i + j]);
+				}
+		}
+	_putchar('\n');
+	}
+	if (size <= 0)
+		putchar('\n');
+}
+
