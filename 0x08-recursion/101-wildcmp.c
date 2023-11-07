7c2dcd4425006dbc71ac62e6a17bf0ff72423052
esraazaghloul <esraazaghloul@live.com>
2023-11-07 15:01:19 -0800

    3

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,31 +10,31 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-        {
-            return (wildcmp(s1, s2 + 1));
-        }
-        else if (*s1 == '\0')
-        {
-            return (wildcmp(s1, s2 + 1));
-        }
-        else
-        {
-            return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-        }
-    }
-    else if (*s1 == '\0')
-    {
-        return (*s2 == '\0');
-    }
-    else if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    else
-    {
-        return (0);
-    }
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		}
+	}
+	else if (*s1 == '\0')
+	{
+		return (*s2 == '\0');
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else
+	{
+		return (0);
+	}
 }
