1d84589f9ab7cccd6ba923acc027d6c4de41e14b
esraazaghloul <esraazaghloul@live.com>
2023-11-07 14:40:30 -0800

    3

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,18 +10,11 @@
 
 int is_palindrome(char *s)
 {
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	if (len <= 1)
+	if (*s == '\0')
 	{
 		return (1);
 	}
-	return (palindrome_helper(s, 0, len - 1));
+	return
 }
 
 /**
