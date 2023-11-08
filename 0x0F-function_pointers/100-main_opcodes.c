a1947c031bd69c1e796f0e6d713ec7da43fa037f
esraazaghloul <esraazaghloul@live.com>
2023-11-08 15:42:52 -0800

    3

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,30 +11,19 @@
  *         2 if the number of bytes is negative
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	char *p - (char *)main;
+	int b;
+
 	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	int num_bytes = atoi(argv[1]);
-
-	if (num_bytes < 0)
-	{
-		printf("Error\n");
-		return (2);
-	}
-
-	unsigned char *main_opcodes = (unsigned char *)main;
-
-	for (int i = 0; i < num_bytes; i++)
-	{
-		printf("%02x ", *(main_opcodes + i));
-	}
-
-	printf("\n");
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
 
 	return (0);
 }
